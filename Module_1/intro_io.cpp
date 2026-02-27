23a662fdf86132f46c9f5adce17fddbe1d1e6337
Chotepong Victor Tangton <65469344+S0UN@users.noreply.github.com>
2026-02-27 17:47:11 -0600

    modeule 1

---
+++ Module_1/intro_io.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "enter three numbers \n";
+    int x{};
+    int y{};
+    int z{};
+    std::cin >> x >> y >> z;
+    std::cout << x << y << z;
+
+
+}
